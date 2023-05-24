77368b83c99b0b6dcaf98aeaee793fdf6b188c1e
Yana Vozniuk <yana.vozniuk@ukr.net>
2023-05-24 15:52:11 +0300

    done
    done

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./index.scss";
+
+function App() {
+  const [count, setCount] = React.useState(0);
+
+  const countPlus = () => {
+    setCount(count + 1);
+  };
+
+  const countMinus = () => {
+    setCount(count - 1);
+  };
+
+  return (
+    <div className="App">
+      <div>
+        <h2>Счетчик:</h2>
+        <h1>{count}</h1>
+        <button onClick={countMinus} className="minus">
+          - Минус
+        </button>
+        <button onClick={countPlus} className="plus">
+          Плюс +
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
